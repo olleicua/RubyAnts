f355336464e810f98e7a2cdc174d43bf62958532
Sam Auciello <olleicua@gmail.com>
2012-02-20 15:09:11 -0500

    pre-merge changes

--- bots/explore.rb
+++ bots/explore.rb
@@ -6,6 +6,10 @@
   Jan 2012     | opensource.org/licenses/MIT
 =end
 
+# CONSTANTS #
+
+Infinity = 1.0 / 0.0
+
 # METHODS #
 
 class Array
@@ -16,6 +20,7 @@ end
 
 class Square
   attr_accessor :visited
+  attr_writer :boringness
   def to_s
     if self.land?
       return " "
@@ -29,6 +34,9 @@ class Square
       return "a"
     end
   end
+  def neighbors
+    [:N, :S, :E, :W].map{|dir| self.neighbor dir}
+  end
   def safe?
     not (self.ant? or self.water?)
   end
@@ -37,6 +45,20 @@ class Square
   end
   def targetable?
     self.safe? and (not @visited)
+  end
+  def getBoringness
+    if @boringness.nil? and (not @searching)
+      @searching = true
+      @boringness = self.calculateBoringness
+      @searching = false
+    end
+    return Infinity if @searching
+    return @boringness
+  end
+  def calculateBoringness
+    return Infinity if self.water?
+    return 0 unless self.visited
+    return self.neighbors.map{|square| square.getBoringness}.min + 1
   end
 end
 
@@ -51,9 +73,13 @@ ai = AI.new
 ai.setup{}
 ai.run do |ai| # this block is executed once for each turn
   
+  Log.write "== turn #{ai.turn_number} ==\n"
+  Log.flush
+  
   # mark successfully visitted squares as visitted
   ai.map.each do |row|
     row.each do |square|
+      square.boringness = nil
       square.visited = true if square.ant? and square.ant.mine?
     end
   end
@@ -62,19 +88,30 @@ ai.run do |ai| # this block is executed once for each turn
   ai.my_ants.each do |ant|
     
     # pick a random safe move
-    move = [:N, :E, :S, :W] \
-      .reject{|dir| ant.square.neighbor(dir).unsafe?} \
-      .random
+    #move = [:N, :E, :S, :W] \
+    #  .reject{|dir| ant.square.neighbor(dir).unsafe?} \
+    #  .random
         
     # pick an unvisitted square if possible
-    [:N, :E, :S, :W].each do |dir|
-      if ant.square.neighbor(dir).targetable?
-        move = dir
-      end
+    #[:N, :E, :S, :W].each do |dir|
+    #  if ant.square.neighbor(dir).targetable?
+    #    move = dir
+    #  end
+    #end
+    move = [:N, :E, :S, :W] \
+      .reject{|dir| ant.square.neighbor(dir).unsafe?} \
+      .min do |dir1, dir2|
+      ant.square.neighbor(dir1).getBoringness <=>
+        ant.square.neighbor(dir2).getBoringness
     end
+    
+    b = [:N, :E, :S, :W].map{|d|ant.square.neighbor(d).getBoringness}
+    
+    Log.write " ant at #{ant.square.row},#{ant.square.col}: #{b}\n"
     
     # order the ant if a valid move exists
     if move
+      ant.square.ant = nil
       ant.square.neighbor(move).ant = ant
       ant.order move
     end
